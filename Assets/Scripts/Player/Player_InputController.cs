863e33b40c3c1b1d4a1da00cf121fad95050fb3e
Eetu Maunuksela <eetu.maunuksela@cs.tamk.fi>
2016-09-21 12:50:02 +0300

    Ground prefabs renamed. Script for handling basic input. Script for basic player movement.

---
+++ Assets/Scripts/Player/Player_InputController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_InputController : MonoBehaviour {
+
+    private float _inputX;
+    private bool _jump;
+
+    private Player_Movement _playerMovement;
+
+	// Use this for initialization
+	void Start () {
+
+        _playerMovement = GetComponent<Player_Movement>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        GetInput();
+
+	}
+
+    private void GetInput() {
+
+        _inputX = Input.GetAxis("Horizontal");
+        _jump = Input.GetButtonDown("Jump");
+
+        if(_inputX < 0 || _inputX > 0)
+        {
+            _playerMovement.Move(_inputX);
+        }
+
+        if (_jump)
+        {
+            _playerMovement.Jump();
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Movement : MonoBehaviour {
+
+    private Transform _playerTransform;
+    private Rigidbody2D _playerRigidbody2D;
+
+    [SerializeField] private float _playerMoveSpeed;
+    [SerializeField] private float _playerJumpForce;
+    [SerializeField] private float _groundCheckRadius;
+    [SerializeField] private Transform _groundCheckTransform;
+
+    private bool _isGrounded;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        _playerTransform = GetComponent<Transform>();
+        _playerRigidbody2D = GetComponent<Rigidbody2D>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        CheckIfGrounded();
+
+        
+	}
+
+    public void Move(float inputX)
+    {
+
+        _playerRigidbody2D.velocity = new Vector2(inputX * _playerMoveSpeed, _playerRigidbody2D.velocity.y);
+
+    }
+
+    public void Jump()
+    {
+        if (_isGrounded)
+        {
+            //_playerRigidbody2D.AddForce(new Vector2(0, _playerJumpForce));
+            _playerRigidbody2D.velocity = new Vector2(_playerRigidbody2D.velocity.x, _playerJumpForce);
+            _isGrounded = false;
+
+        }
+
+    }
+
+    private void CheckIfGrounded()
+    {
+        _isGrounded = false;
+
+        var colliders = Physics2D.OverlapCircleAll(_groundCheckTransform.position,  _groundCheckRadius);
+
+        for(int i = 0; i < colliders.Length; i++)
+        {
+            if(colliders[i].tag == "Ground")
+            {
+
+                _isGrounded = true;
+            }
+        }
+
+
+    }
+
+}
