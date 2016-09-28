2a01fb333194b583ad8023cc84be2ee648d4a0cc
Eetu Maunuksela <eetu.maunuksela@cs.tamk.fi>
2016-09-28 11:18:17 +0300

    Hero prefab values fixed Scripts Goblin_AI & Goblin_Move (broken)

---
+++ Assets/Scripts/Enemies/Goblin_AI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goblin_AI : MonoBehaviour {
+
+    private Goblin_Move Goblin_Move;
+
+    private bool _aggro = false;
+
+	// Use this for initialization
+	void Start () {
+
+        Goblin_Move = GetComponent<Goblin_Move>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Goblin_Move.MoveToPlayer(other.gameObject.transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/Goblin_Move.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goblin_Move : MonoBehaviour {
+
+    [SerializeField]
+    private float movementSpeed;
+    private Transform _transform;
+    private Rigidbody2D _rigidBody;
+
+	// Use this for initialization
+	void Start () {
+
+        _transform = GetComponent<Transform>();
+        _rigidBody = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void MoveToPlayer(Vector3 playerPos)
+    {
+        Vector3 moveTo = playerPos - _transform.position;
+
+        _transform.position += new Vector3(Mathf.Lerp(_transform.position.x, playerPos.x, Time.deltaTime * _), _transform.position.y, _transform.position.z);
+    }
+}
