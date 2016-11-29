014b831f45c943d9a726b40e48b5f15f203150d8
WPK\c5vpulkk <ok>
2016-11-29 15:40:00 +0200

    Added level 8

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -97,14 +97,14 @@ namespace CallOfValhalla
         //For testing purposes
         public void EetunSceneen()
         {
-            GameManager.Instance.Level = 8;
+            GameManager.Instance.Level = 9;
             GameManager.Instance.Game();
         }
 
         //For testing purposes
         public void TeemunSceneen()
         {
-            GameManager.Instance.Level = 9;
+            GameManager.Instance.Level = 10;
             GameManager.Instance.Game();
         }
 
