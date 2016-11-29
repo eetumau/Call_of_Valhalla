b0e6f19fdcb1a5d039e77ca88694bd7534c014c2
LENOVO-PC\santeri <santeri.orava@hotmail.com>
2016-11-29 12:59:40 +0200

    -Trolls luk (very) ded nu

---
+++ Assets/Scripts/Enemies/Troll_Decapitation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Troll_Decapitation : MonoBehaviour {
+
+	public GameObject head;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void Decapitation () {
+		var t = transform;
+
+		Instantiate(head, t.position + Vector3.up * 2F,Quaternion.identity);
+	}
+
+}
