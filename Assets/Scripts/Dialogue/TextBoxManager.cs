e22add1977d67d858ab009670079adcb89708085
Teemu Arponen <teemu.arponen@cs.tamk.fi>
2016-12-13 00:18:13 +0200

    starting the dialogue system. Not much, but it's something

---
+++ Assets/Scripts/Dialogue/TextBoxManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextBoxManager : MonoBehaviour {
+
+    [SerializeField]
+    private TextAsset _textFile;
+    [SerializeField]
+    private string[] _textLines;
+
+	// Use this for initialization
+	void Start () {
+	
+	}    
+
+    // Use this for initialization
+    void Awake()
+    {
+
+        if (_textFile != null)
+        {
+            _textLines = (_textFile.text.Split('\n'));
+        }
+    }
+}
