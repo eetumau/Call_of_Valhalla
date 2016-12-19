e802eb3a33b23a1257f6970c2283f4a8a9296a54
WPK\c5tarpon <teemu.arponen@cs.tamk.fi>
2016-12-19 11:01:21 +0200

    More work on dialogues

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CallOfValhalla.Dialogue
+{
+    public class NPC : MonoBehaviour
+    {
+        private Sprite _odinSprite;
+        private Sprite _thorSprite;
+        private Sprite _thorHammerSprite;
+        
+
+        private SpriteRenderer _renderer;
+        // Use this for initialization
+        void Awake()
+        {
+            _thorSprite = Resources.Load("NPC/Thor", typeof(Sprite)) as Sprite;
+            _odinSprite = Resources.Load("NPC/Odin", typeof(Sprite)) as Sprite;
+            _thorHammerSprite = Resources.Load("NPC/Thor_1", typeof(Sprite)) as Sprite;
+            _renderer = GetComponent<SpriteRenderer>();
+        }
+
+        public void SetOdin()
+        {
+            _renderer.sprite = _odinSprite;
+        }
+
+        public void SetThor()
+        {
+            _renderer.sprite = _thorSprite;
+        }
+
+        public void SetThorHammer()
+        {
+            _renderer.sprite = _thorHammerSprite;
+        }
+    }
+}
