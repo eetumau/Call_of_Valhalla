192ba640ca2c2524ccc1db467c46c0cb59c492c6
LENOVO-PC\santeri <santeri.orava@hotmail.com>
2017-01-09 16:38:17 +0200

    -Stufferino Zoomerino

---
+++ Assets/Scripts/BossZoom.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using CallOfValhalla.Player;
+
+public class BossZoom : MonoBehaviour {
+
+	private Player_CameraFollow _camera;
+
+	// Use this for initialization
+	void Awake () {
+		_camera = FindObjectOfType<Player_CameraFollow>();
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter2D (Collider2D other) {
+		if (other.tag == "Player")
+			_camera.IncreaseCamera();
+	}
+}
