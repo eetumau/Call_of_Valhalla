9df9ca86390625c89094695576db9377741c93ca
WPK\c5tarpon <teemu.arponen@cs.tamk.fi>
2017-01-10 14:13:36 +0200

    New camera effect which shows opening doors after bosses

---
+++ Assets/Scripts/Enemies/BossCameraEffect.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using CallOfValhalla.Enemy;
+using CallOfValhalla.Player;
+
+public class BossCameraEffect : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _boss;
+    [SerializeField]
+    private float _cameraSpeed;
+    [SerializeField]
+    private float _cameraResetTime;
+    [SerializeField]
+    private float _cameraDelay;
+
+    private Player_CameraFollow _cameraScript;
+    private Player_InputController _playerInput;
+    private Enemy_HP _enemyHp;
+    private Fenrir_HP _fenrirHp;
+    private bool _bossDead;
+
+	// Use this for initialization
+	void Awake ()
+	{
+	    _enemyHp = _boss.GetComponent<Enemy_HP>();
+	    _playerInput = FindObjectOfType<Player_InputController>();
+        
+
+	    if (_enemyHp == null)
+	    {
+            Debug.Log("NUULLL");
+	        _fenrirHp = _boss.GetComponent<Fenrir_HP>();
+	    }
+	    _cameraScript = FindObjectOfType<Player_CameraFollow>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (_enemyHp != null)
+	    {
+	        if (_enemyHp.HP <= 0 && !_bossDead)
+	        {
+	            _bossDead = true;
+	            _cameraScript.DecreaseCameraSlowly();
+                _playerInput.DisableControls(true);
+	            StartCoroutine(Delay());
+	        }
+	    }
+	    else
+	    {
+            if (_fenrirHp.HP <= 0 && !_bossDead)
+            {
+                _bossDead = true;
+                _cameraScript.DecreaseCameraSlowly();
+                _playerInput.DisableControls(true);
+                StartCoroutine(Delay());
+            }
+        }
+	    
+	}
+
+    private IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(_cameraDelay);
+        _cameraScript.MoveCamerahere(transform.position, _cameraSpeed, _cameraResetTime);
+    }
+}
